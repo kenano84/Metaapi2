1adf990af051961435a1a5a59d124da2a38d4c48
Alice <alice.morell@outlook.com>
2020-12-07 15:25:54 +0100

    changed routes

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ import controller.users_controller as usc
 import controller.apis_controller as ac
 
 
+
 @app.route("/")
 def index():
     return "Welcome"
@@ -20,26 +21,26 @@ def get_all_users():
 def add_user():
     data_string = request.get_json()
     usc.add_user(data_string)
-    return json.dumps({'status': 'Success'}), 201, {'ContentType': 'application/json'}
+    return json.dumps({'Success': True}), 201, {'ContentType': 'application/json'}
 
 
-@app.route('/users/<string:id>', methods=['GET'])
+@app.route('/users/<string:id>', methods=['GET', 'DELETE', 'PUT'])
 def get_user_by_id(id):
     user = usc.get_user_by_id(id)
-    return json.dumps(user.to_dict()), 200, {'ContentType': 'application/json'}
+    return json.dumps(user.to_dict()), {'ContentType': 'application/json'}
 
 
-@app.route('/users/<string:id>', methods=['DELETE'])
 def delete_user_by_id(id):
-    usc.delete_user(id)
-    return json.dumps({'status': 'User deleted'}), 200, {'ContentType': 'application/json'}
+    user = usc.delete_user(id)
+    response = Response("", status=404, mimetype='application/json')
+    return response
 
 
-@app.route('/users/<string:id>', methods=['PUT'])
 def put_user_by_id(id):
     request_data = request.get_json()
     usc.change_entire_user_info(id, request_data)
-    return json.dumps({"status": "Complete user update performed"}), 204, {'ContentType': 'application/json'}
+    response = Response(json.dumps({"status": "Complete user update performed"}, status=204))
+    return response
 
 
 @app.route('/users/<string:id>', methods=['PATCH'])
@@ -51,18 +52,13 @@ def patch_user(id):
 
 # ALL API ROUTES
 
-@app.route('/users/<string:id>/apis', methods=['GET'])
+@app.route('/apis/<string:id>', methods=['GET'])
 def get_all_apis_for_user(id):
     apis = ac.get_all_apis(id)
     return Response(json.dumps(apis), mimetype='application/json')
 
 
-@app.route('/apis/<string:id>', methods=['GET'])
-def get_api(id):
-    pass
-
-
-@app.route('/users/<string:id>/apis/', methods=['POST'])
+@app.route('/apis/<string:id>', methods=['POST'])
 def add_api_to_user(id):
     data = request.get_json()
     ac.add_api(id, data)
