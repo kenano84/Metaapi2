1a86c592ba117f8cae2f7b50cf3934d707dd4748
Alice <alice.morell@outlook.com>
2020-12-07 10:32:55 +0100

    change routes

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,8 @@
 from app import app
 from flask import Response, request
 import json
-<<<<<<< HEAD
 import controller.users_controller as usc
 import controller.apis_controller as ac
-=======
-import controllers.users_controller as usc
->>>>>>> 27c13c892ce1ccbcd435bc03d1ca148967f97009
 
 
 @app.route("/")
@@ -24,38 +20,26 @@ def get_all_users():
 def add_user():
     data_string = request.get_json()
     usc.add_user(data_string)
-    return json.dumps({'Success': True}), 201, {'ContentType': 'application/json'}
+    return json.dumps({'status': 'Success'}), 201, {'ContentType': 'application/json'}
 
 
-@app.route('/users/<string:id>', methods=['GET', 'DELETE', 'PUT'])
+@app.route('/users/<string:id>', methods=['GET'])
 def get_user_by_id(id):
     user = usc.get_user_by_id(id)
-    return json.dumps(user.to_dict()), {'ContentType': 'application/json'}
+    return json.dumps(user.to_dict()), 200, {'ContentType': 'application/json'}
 
 
+@app.route('/users/<string:id>', methods=['DELETE'])
 def delete_user_by_id(id):
-    user = usc.delete_user(id)
-    response = Response("", status=404, mimetype='application/json')
-    return response
+    usc.delete_user(id)
+    return json.dumps({'status': 'User deleted'}), 200, {'ContentType': 'application/json'}
 
 
+@app.route('/users/<string:id>', methods=['PUT'])
 def put_user_by_id(id):
     request_data = request.get_json()
     usc.change_entire_user_info(id, request_data)
-    response = Response(json.dumps({"status": "Complete user update performed"}, status=204))
-    return response
-
-
-    # vad har koden nedan för syfte? att stoppa in den på rätt id-rad? tänker att vi inte behöver
-    # det iom att vi skickar in till databasen samma primary key... men kanske tänker fel
-    i = 0;
-    for user in users:
-        current_id = user["id"]
-    if current_id == id():
-            user[i] = new_user_id
-            i += 1
-    response = Response("", status=204)
-    return response
+    return json.dumps({"status": "Complete user update performed"}), 204, {'ContentType': 'application/json'}
 
 
 @app.route('/users/<string:id>', methods=['PATCH'])
@@ -67,13 +51,18 @@ def patch_user(id):
 
 # ALL API ROUTES
 
-@app.route('/apis/<string:id>', methods=['GET'])
+@app.route('/users/<string:id>/apis', methods=['GET'])
 def get_all_apis_for_user(id):
     apis = ac.get_all_apis(id)
     return Response(json.dumps(apis), mimetype='application/json')
 
 
-@app.route('/apis/<string:id>', methods=['POST'])
+@app.route('/apis/<string:id>', methods=['GET'])
+def get_api(id):
+    pass
+
+
+@app.route('/users/<string:id>/apis/', methods=['POST'])
 def add_api_to_user(id):
     data = request.get_json()
     ac.add_api(id, data)
